54af0a69ea21905f535eba0b9f41be14be2b9dd1
devxoul <devxoul@gmail.com>
2012-07-30 15:52:15 +0900

    Leave comments

--- prototypes/SanchoPrototypePythonEmbedding/SanchoPrototypePythonEmbedding/main.c
+++ prototypes/SanchoPrototypePythonEmbedding/SanchoPrototypePythonEmbedding/main.c
@@ -10,33 +10,44 @@
 #include <Python/Python.h>
 
 int main(int argc, const char * argv[])
-{	
+{
 	const char *pluginDir = "/Users/xoul/workspace/Sancho/prototypes/SanchoPrototypePythonEmbedding/plugins";
 	
+	// Initialize python interpreter.
 	Py_Initialize();
 	
 	PyObject *sysPath = PySys_GetObject( "path" );
 	PyObject *path = PyString_FromString( pluginDir );
 	PyList_Insert( sysPath, 0, path );
 	
+	// Import python moudule named 'helloworld'
 	PyObject *module = PyImport_ImportModule( "helloworld" );
 	if( PyErr_Occurred() )
 		PyErr_Print();
 	
+	// Get dictionary from module 'helloworld'
 	PyObject *dict = PyModule_GetDict( module );
 	Py_DECREF( module );
 	
+	// Get method named 'init' from dictionary.
 	PyObject *func = PyDict_GetItemString( dict, "init" );
 	
+	// Check method from dictionary is callable.
 	if( PyCallable_Check( func ) )
 	{
+		// Arguments are passed as tuple.
+		// Make a tuple of length 1.
 		PyObject *arguments = PyTuple_New( 1 );
+		
+		// Set first item to 'Hello'
 		PyTuple_SetItem( arguments, 0, PyString_FromString( "Hello" ) );
 		
+		// Call method with arguments and get return value.
 		PyObject *result = PyObject_CallObject( func, arguments );
 		Py_DECREF( func );
 		Py_DECREF( arguments );
 		
+		// Print return value as string.
 		printf( "%s\n", PyString_AsString( result ) );
 		Py_DECREF( result );
 	}
@@ -46,6 +57,7 @@ int main(int argc, const char * argv[])
 		PyErr_Print();
 	}
 	
+	// Finalize python interpreter.
 	Py_Finalize();
 	
 	return 0;
